f5e9de571837be1e84b2469bf498ceb4025807ba
Andy Shora <andyshora@gmail.com>
2011-09-24 23:46:30 +0100

    added mongodb module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var mongo = require('mongodb');
 
 var server = http.createServer(function(request, response){
   
@@ -47,6 +48,30 @@ var server = http.createServer(function(request, response){
 });
 server.listen(8080);
 
+/*
+var client = new mongo.Db('test', new mongo.Server("127.0.0.1", 27017, {})),
+    test = function (err, collection) {
+      collection.insert({a:2}, function(err, docs) {
+
+        collection.count(function(err, count) {
+          test.assertEquals(1, count);
+        });
+
+        // Locate all the entries using find
+        collection.find().toArray(function(err, results) {
+          test.assertEquals(1, results.length);
+          test.assertTrue(results.a === 2);
+
+          // Let's close the db
+          client.close();
+        });
+      });
+    };
+
+client.open(function(err, p_client) {
+  client.collection('test_insert', test);
+});
+*/
 
 
 var nowjs = require("now");
